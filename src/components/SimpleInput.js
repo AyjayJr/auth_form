c3b3468ed0da60b88989b271ca33ef635bf646ef
AyjayJr <ayjayjr@hotmail.com>
2023-03-28 20:46:23 -0400

    capturing input from the input form

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,32 @@
+import { useRef, useState } from 'react';
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState('');
+
+  const inputChangeHandler = (e) => {
+    setEnteredName(e.target.value);
+    console.log(enteredName);
+  };
+
+  const formSubmissionHandler = (e) => {
+    e.preventDefault();
+
+    // nameInputRef.current.value = ''; // NOT IDEAL, DON'T MANIPULATE DOM
+    setEnteredName('');
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+          ref={nameInputRef}
+          onChange={inputChangeHandler}
+          type='text'
+          id='name'
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
